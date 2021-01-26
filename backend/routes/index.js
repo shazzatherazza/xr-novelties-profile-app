23ebbb77b73599612a05da98c8fbc3d2317ba127
shazzatherazza <52185121+shazzatherazza@users.noreply.github.com>
2021-01-26 18:13:39 +0800

    Initial commit

---
+++ backend/routes/index.js
@@ -0,0 +1,97 @@
+var express = require("express");
+var router = express.Router();
+var User = require("../models/user");
+var mid = require("../middleware");
+// GET /logout
+router.get("/logout", function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect("/login");
+      }
+    });
+  }
+});
+// GET /profile
+router.get("/profile", mid.requiresLogin, function(req, res, next) {
+  User.findById(req.session.userId).exec(function(error, user) {
+    if (error) {
+      return next(error);
+    } else {
+      return res.json({
+        title: "Profile",
+        name: user.name,
+        role: user.role
+      });
+    }
+  });
+});
+
+// GET /login
+router.get("/login", mid.loggedOut, function(req, res, next) {
+  return res.json({ title: "Log In" });
+});
+// POST /login
+router.post("/login", function(req, res, next) {
+  if (req.body.email && req.body.password) {
+    User.authenticate(req.body.email, req.body.password, function(error, user) {
+      if (error || !user) {
+        var err = new Error("Wrong email or password.");
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect("/profile");
+      }
+    });
+  } else {
+    var err = new Error("Email and password are required.");
+    err.status = 401;
+    return next(err);
+  }
+});
+// GET /register
+router.get("/register", mid.loggedOut, function(req, res, next) {
+  return res.json({ title: "Sign Up" });
+});
+// POST /register
+router.post("/register", function(req, res, next) {
+  if (
+    req.body.email &&
+    req.body.name &&
+    req.body.role &&
+    req.body.password &&
+    req.body.confirmPassword
+  ) {
+    // confirm that the user typed the same password twice
+    if (req.body.password !== req.body.confirmPassword) {
+      var err = new Error("Passwords do not match.");
+      err.status = 400;
+      return next(err);
+    }
+    // create object with form input
+    var userData = {
+      email: req.body.email,
+      name: req.body.name,
+      role: req.body.role,
+      password: req.body.password
+    };
+    User.create(userData, function(error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect("/profile");
+      }
+    });
+  } else {
+    var err = new Error("All fields required.");
+    err.status = 400;
+    return next(err);
+  }
+});
+
+module.exports = router;
